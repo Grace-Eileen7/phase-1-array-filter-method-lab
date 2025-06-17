c1653e72382aa12ebe98f5e974225ff930652830
Grace Eileen Bass <grace.eilee.bass@gmail.com>
2025-06-18 00:40:52 +0300

    Add functions using filter(): findMatching, fuzzyMatch, matchName

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+//1.findMatching(drivers, query)
+function findMatching(drivers, query) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === query.toLowerCase()
+  );
+} // to return all drivers whose names match the query, case-insensitively.
+
+//2.fuzzyMatch(drivers, query)
+function fuzzyMatch(drivers, query) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(query.toLowerCase())
+  );
+} //Returns all drivers whose names start with the query string, case-insensitively.
+
+//3. matchName(drivers, query)
+function matchName(drivers, query) {
+  return drivers.filter((driver) => driver.name === query);
+} // Returns all driver objects where the name property exactly matches the query string.
+
+/* const drivers = [
+  { name: "Bobby", hometown: "New York" },
+  { name: "Sammy", hometown: "Los Angeles" },
+  { name: "Sally", hometown: "Cleveland" },
+  { name: "Annette", hometown: "Chicago" },
+  { name: "Bobby", hometown: "Tampa Bay" }
+];
+
+findMatching(["Bobby", "Sammy", "Sally"], "bobby"); 
+// → ["Bobby"]
+
+fuzzyMatch(["Bobby", "Sammy", "Sally"], "Sa"); 
+// → ["Sammy", "Sally"]
+
+matchName(drivers, "Bobby"); 
+// → [
+//   { name: "Bobby", hometown: "New York" },
+//   { name: "Bobby", hometown: "Tampa Bay" }
+// ]
+*/
